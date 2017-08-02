e85dc7a6a21caa6af32e09321a753f2a5fffe76b
Kristin <kmtreder@hawaii.edu>
2017-08-01 14:08:35 -1000

    inital commit

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const Users = require('./models/users');
 // require packages
 
+//preferred backend way to create a server
+//express wants to be very slim (routing and middleware) and therefore includes no native way to do authentication -- add functionality by adding middleware
+const express = require('express');
+
+//bc receiving data from front-end
+const bodyParser = require('body-parser');
+//express's package for handling authentication
+//middleware passport.js.org
+const passport = require('passport');
+//a "strategy" of passport that is baked in
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+//THEN need to npm install --save express body-parser passport passport-local express-session
+
 // invoke express
 
 // add middleware
